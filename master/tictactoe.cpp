d491769b03195f78794701901840c4fe3683abc0
ThrosturX <throstur@ubuntu.(none)>
2012-10-26 08:44:41 -0700

    tic

---
+++ master/tictactoe.cpp
@@ -0,0 +1,60 @@
+#inculde "tictactoe.h"
+
+using namespace std;
+
+// Constructar, initializes scores as 0-0-0 (player 1, player 2, tie)
+TicTacToe::TicTacToe()
+{
+
+}
+
+// Plays the game
+void TicTacToe::play()
+{
+
+}
+
+// prints the board and defines player 't' as winner
+// adds 1 to player t's score
+void TicTacToe::endGame(char t)
+{
+
+}
+
+// places player s's symbol on the board
+void TicTacToe::addSymbol(char s, int x, int y)
+{
+
+}
+
+// prints the board
+void TicTacToe::print()
+{
+
+}
+
+// clears the board
+void TicTacToe::clear()
+{
+
+}
+
+// checks for a winner, player s is the winner
+bool TicTacToe::winner(char &s)
+{
+
+}
+
+// determine if the board is full
+bool TicTacToe::fullBoard()
+{
+
+}
+
+// logic for a player to move
+void TicTacToe::playerMove(char &p)
+{
+	
+}
+
+
---
+++ master/tictactoe.h
@@ -0,0 +1,24 @@
+#ifndef TICTACTOE_H
+#define TICTACTOE_H
+
+#include <iostream>
+
+class TicTacToe
+{
+public:
+	TicTacToe();
+	void addSymbol(char symbol, int x, int y);
+	void print();
+	void clean();
+	void play();
+
+private:
+	char array[3][3];
+	int score[3];
+	bool winner(char &s);
+	bool fullBoard();
+	void playerMove(char &p);
+	void endGame(char t);
+};
+
+#endif // BOARD_H
