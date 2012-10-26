50b7d28be3359be118ec077e7ac439166cc4bc6a
ThrosturX <throstur@ubuntu.(none)>
2012-10-26 09:27:19 -0700

    automated commit

--- master/main.cpp
+++ master/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "tictactoe.h"
 
+/// TODO:
+/// EVERYONE ADD THEIR SOURCE FILE HERE:
 #include "throstur.cpp"
 #include "hlynur.cpp"
 
@@ -11,7 +13,8 @@ int main()
 /*	TicTacToe board;
 	board.play();
 */
-
+/// TODO:
+/// EVERYONE ADD THEIR FUNCTION HERE:
 	Throstur();
 	printHlynur();
 
